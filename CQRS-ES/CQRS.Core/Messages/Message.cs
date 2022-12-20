42e9ddf5af9e702ce9f3b5e9dfe4adc27475990c
Vitor Nicacio <vitornicacio10@hotmail.com>
2022-12-20 11:09:28 -0300

    feat: add commands classes

---
+++ CQRS-ES/CQRS.Core/Messages/Message.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQRS.Core.Commands;
+public abstract class Message
+{
+    public Guid Id { get; set; }
+}
