adcc231717563d5aa0ff644702b0bdbb253bc53e
Vitor Nicacio <vitornicacio10@hotmail.com>
2022-12-28 19:24:26 -0300

    feat: add domain entities

---
+++ SM-Post/Post.Query/Post.Query.Domain/Entities/CommentEntity.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Post.Query.Domain.Entities
+{
+    [Table("Comment")]
+    public class CommentEntity
+    {
+        public Guid CommentId { get; set; }
+
+        public string Username { get; set; }
+
+        public DateTime CommentDate { get; set; }
+
+        public string Comment { get; set; }
+
+        public bool Edited { get; set; }
+
+        public Guid PostId { get; set; }
+
+        [System.Text.Json.Serialization.JsonIgnore]
+        public virtual PostEntity Post { get; set; }
+    }
+}
