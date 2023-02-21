fc888ed42a0f0f2d20b25129f84420013289b54d
Vitor Nicacio <vitornicacio10@hotmail.com>
2023-02-21 16:22:42 -0300

    feat: Query implementation from CQRS

---
+++ SM-Post/Post.Query/Post.Query.Api/Queries/QueryHandler.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Post.Query.Domain.Entities;
+using Post.Query.Domain.Repositories;
+
+namespace Post.Query.Api.Queries
+{
+    public class QueryHandler : IQueryHandler
+    {
+        private readonly IPostRepository _postRepository;
+
+        public QueryHandler(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        public async Task<List<PostEntity>> HandleAsync(FindAllPostsQuery query)
+        {
+            _ = query ?? throw new ArgumentNullException(nameof(query));
+            return await _postRepository.ListAllAsync();
+        }
+
+        public async Task<List<PostEntity>> HandleAsync(FindPostByIdQuery query)
+        {
+            _ = query ?? throw new ArgumentNullException(nameof(query));
+            var post = await _postRepository.GetByIdAsync(query.Id);
+
+            return new List<PostEntity>(){ post };
+        }
+
+        public async Task<List<PostEntity>> HandleAsync(FindPostsByAuthor query)
+        {
+            _ = query ?? throw new ArgumentNullException(nameof(query));
+            return await _postRepository.ListBytAuthorAsync(query.Author);
+        }
+
+        public async Task<List<PostEntity>> HandleAsync(FindPostsWithCommentsQuery query)
+        {
+            _ = query ?? throw new ArgumentNullException(nameof(query));
+            return await _postRepository.ListWithCommentsAsync();
+        }
+
+        public async Task<List<PostEntity>> HandleAsync(FindPostsWithLikesQuery query)
+        {
+            _ = query ?? throw new ArgumentNullException(nameof(query));
+            return await _postRepository.ListWithLikesAsync(query.NumberOfLikes);
+        }
+    }
+}
